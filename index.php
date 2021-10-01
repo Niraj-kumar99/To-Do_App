d7aa6664658d4d3ddfb1f26aaad59a257b40cab8
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-10-01 11:39:15 +0530

    refactor: added while loop to fetch tasks from DB , POST method , added logic to checkbox

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require 'db_connection.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,18 +14,43 @@
 <body>
     <div class="main-section">
         <div class="add-section">
-            <form action="">
+            <form action="" method="POST" autocomplete="off">
                 <input type="text" name="title" placeholder="Can not be empty">
                 <button type="submit">Add &nbsp; <span>&#43;</span></button>
             </form>
         </div>
+        <?php 
+            $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+        ?>
         <div class="show-todo-section">
+            <?php if($todos->fetch_row() === 0){ ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="assets/f.png" width="100%" alt="">
+                        <img src="assets/Ellipsis.gif" width="80px">
+                    </div>
+                </div>
+            <?php } ?>
+
+            <?php while($todo = $todos->fetch_assoc()) {?>
             <div class="todo-item">
-                <input type="checkbox">
-                <h2>This is Niraj</h2>
+                <span id="<?php echo $todo['id']; ?>"
+                      class="remove-to-do">x</span>
+                <?php if($todo['checked']){ ?>
+                    <input type="checkbox"
+                           class="check-box"
+                           checked>
+                    <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                <?php } 
+                    else {?>
+                    <input type="checkbox"
+                           class="check-box">
+                    <h2><?php echo $todo['title']?></h2>
+                <?php } ?>
                 <br>
-                <small>created: 30/09/2021</small>
+                <small>created: <?php echo $todo['date_time']?></small>
             </div>
+            <?php } ?>
         </div>
     </div>
 </body>
